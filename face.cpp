3517c9c39ac908418295537f5e054819df475fb8
Andreas Georgiou <a.georgiou.2014@bristol.ac.uk>
2016-11-01 17:48:14 +0000

    Added CMakeLists.txt. Remember to name the project folder as DartBoardDetector

--- face.cpp
+++ face.cpp
@@ -7,11 +7,12 @@
 // header inclusion
 // header inclusion
 #include <stdio.h>
-#include "opencv2/objdetect/objdetect.hpp"
-#include "opencv2/opencv.hpp"
-#include "opencv2/core/core.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+//#include "opencv2/objdetect/objdetect.hpp"
+//#include "opencv2/opencv.hpp"
+//#include "opencv2/core/core.hpp"
+//#include "opencv2/highgui/highgui.hpp"
+//#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/opencv.hpp>
 #include <iostream>
 #include <stdio.h>
 
