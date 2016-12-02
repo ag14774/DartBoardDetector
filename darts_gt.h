7abe874c4cf61b108a1cd2650d7f1ca78837a229
Constantinos Theophilou <ct14872@my.bristol.ac.uk>
2016-12-02 15:59:10 +0000

    Added necessary groud truths and added a few things in the report.

--- darts_gt.h
+++ darts_gt.h
@@ -17,22 +17,22 @@ String cascade_name = "dartcascade/cascade.xml";
 
 Rect dart0gt[1] = {Rect(427,0,193,215)};
 Rect dart1gt[1] = {Rect(170,108,248,246)};
-Rect dart2gt[1] = {Rect(93,87,108,110)};
+Rect dart2gt[2] = {Rect(93,87,108,110),Rect(318,57,67,40)};
 Rect dart3gt[1] = {Rect(316,142,83,91)};
 Rect dart4gt[1] = {Rect(160,67,205,213)};
 Rect dart5gt[1] = {Rect(418,128,134,136)};
 Rect dart6gt[1] = {Rect(204,109,76,80)};
 Rect dart7gt[1] = {Rect(237,152,165,184)};
 Rect dart8gt[2] = {Rect(832,205,141,147),Rect(65,244,68,109)};
-Rect dart9gt[1] = {Rect(171,18,296,295)};
+Rect dart9gt[2] = {Rect(171,18,296,295),Rect(159,516,66,73)};
 Rect dart10gt[3] = {Rect(80,91,117,136),Rect(580,119,65,103),Rect(914,143,39,77)};
-Rect dart11gt[3] = {Rect(168,94,73,86),Rect(217,251,15,25),Rect(438,100,38,85)};
+Rect dart11gt[2] = {Rect(168,94,73,86),Rect(438,100,38,85)};
 Rect dart12gt[1] = {Rect(153,60,71,171)};
 Rect dart13gt[1] = {Rect(260,107,159,163)};
 Rect dart14gt[2] = {Rect(106,85,156,158),Rect(975,80,151,155)};
 Rect dart15gt[1] = {Rect(155,39,148,173)};
 
-int sizes[16] = {1,1,1,1,1,1,1,1,2,1,3,3,1,1,2,1};
+int sizes[16] = {1,1,2,1,1,1,1,1,2,2,3,2,1,1,2,1};
 Rect* positives[16] = {dart0gt,  dart1gt,  dart2gt,  dart3gt,
                        dart4gt,  dart5gt,  dart6gt,  dart7gt,
                        dart8gt,  dart9gt,  dart10gt, dart11gt,
